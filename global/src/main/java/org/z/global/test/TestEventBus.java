cddfe2a8e7d7d5fbd56f68f6663c4d3275274566
jcr34356532 <zzxstar@vip.qq.com>
2014-06-23 16:37:51 +0800

    eventBus

--- global/src/main/java/org/z/global/test/TestEventBus.java
+++ global/src/main/java/org/z/global/test/TestEventBus.java
@@ -7,12 +7,9 @@ import com.google.common.eventbus.EventBus;
 public class TestEventBus {
     @Test
     public void testReceiveEvent() throws Exception {
-
         EventBus eventBus = new EventBus("test");
         EventListener listener = new EventListener();
-
         eventBus.register(listener);
-
         eventBus.post(new TestEvent(200));
         eventBus.post(new TestEvent(300));
         eventBus.post(new TestEvent(400));
