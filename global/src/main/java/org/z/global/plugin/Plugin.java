954ae3d570499592a45b5aea1762c97ac67e1ecc
jcr34356532 <zzxstar@vip.qq.com>
2014-06-28 15:00:11 +0800

    commit

--- global/src/main/java/org/z/global/plugin/Plugin.java
+++ global/src/main/java/org/z/global/plugin/Plugin.java
@@ -1,35 +1,10 @@
-/*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
-package org.z.global.discovery;
+package org.z.global.plugin;
 
 
 import java.util.Collection;
 import java.util.HashMap;
 
-/**
- * An extension point allowing to plug in custom functionality.
- * <p/>
- * A plugin can be dynamically injected with {@link Module} by implementing <tt>onModule(AnyModule)</tt> method
- * removing the need to override {@link #processModule(org.elasticsearch.common.inject.Module)} and check using
- * instanceof.
- */
 public interface Plugin {
 
     /**
--- global/src/main/java/org/z/global/plugin/PluginUtils.java
+++ global/src/main/java/org/z/global/plugin/PluginUtils.java
@@ -1,4 +1,4 @@
-package org.z.global.discovery;
+package org.z.global.plugin;
 
 import java.io.File;
 import java.io.IOException;
