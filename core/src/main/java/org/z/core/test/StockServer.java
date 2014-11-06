f33eb3564cc2b7a1195e650123a78dd0cd1c620a
jcr34356532 <zzxstar@vip.qq.com>
2014-11-06 15:38:31 +0800

    dumpProxy

---
+++ core/src/main/java/org/z/core/test/StockServer.java
@@ -0,0 +1,59 @@
+package org.z.core.test;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.CharsetUtil;
+
+import org.z.core.stocket.FileServerHandler;
+
+public class StockServer {
+	
+	
+	
+	public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new ChannelInitializer<SocketChannel>() {
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                     ChannelPipeline p = ch.pipeline();
+                     p.addLast(
+                             new StringEncoder(CharsetUtil.UTF_8),
+                             new LineBasedFrameDecoder(8192),
+                             new StringDecoder(CharsetUtil.UTF_8),
+                             new ChunkedWriteHandler(),
+                             new FileServerHandler());
+                 }
+             });
+            ChannelFuture f = b.bind(19886).sync();
+
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+	}
+
+}
