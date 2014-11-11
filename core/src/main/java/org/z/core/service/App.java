e25d134977dd28574fa3a5de6d8a63108f91050d
jcr34356532 <zzxstar@vip.qq.com>
2014-11-11 10:51:31 +0800

    app

--- core/src/main/java/org/z/core/service/App.java
+++ core/src/main/java/org/z/core/service/App.java
@@ -11,7 +11,9 @@ import org.z.core.module.ModuleUser;
 import org.z.core.queue.ModuleQueue;
 import org.z.core.stocket.ModuleSocket;
 import org.z.global.dict.Global;
+import org.z.global.environment.Config;
 import org.z.global.factory.ModuleFactory;
+import org.z.global.util.StringUtil;
 
 public class App {
 	protected static Logger logger = LoggerFactory.getLogger(App.class);
@@ -25,6 +27,34 @@ public class App {
 		ModuleFactory.registerModule("appoint", ModuleAppoint.class);
 		ModuleFactory.registerModule("bill", ModuleBill.class);
 		ModuleFactory.registerModule("user", ModuleUser.class);
+	}
+	
+	public static void main(String[] args) {
+		long start  =System.currentTimeMillis();
+		init();
+		logger.info("java.library.path[{}]",new Object[]{System.getProperty("java.library.path")});
+		ModuleFactory.registerLoadModule("");
+		ModuleFactory.registerLoadModule("");
+		ModuleFactory.registerLoadModule("");
+		ModuleFactory.registerLoadModule("");
+		ModuleFactory.registerLoadModule("");
+		String value = Config.rock().getItem("AppServices", "").toLowerCase();
+		String[] values = value.split("\\,");
+		for (int i = 0; i < values.length; i++) {
+			if (StringUtil.isEmpty(values[i])) {
+				continue;
+			}
+			String[] split = values[i].split(":");
+			int length = split.length;
+			if (length == 1) {
+				ModuleFactory.registerLoadModule(split[0]);
+			} else {
+				String[] ips = new String[length - 1];
+				System.arraycopy(split, 1, ips, 0, ips.length);
+				ModuleFactory.registerLoadModule(split[0], ips);
+			}
+		}
+		logger.info("AppServer starting success in {} ms.", System.currentTimeMillis() - start);
 	}
 
 }
